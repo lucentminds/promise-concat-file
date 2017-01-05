76ad94bb87c73d415be4dd35684adb170edaa09d
Scott Johnson <scott@lifecorp.com>
2017-01-05 11:45:11 -0600

    Initial file commit.

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # promise-concat-file
 NodeJs module that concatenates a list of files.
+
+
+### Useage:
+
+```js
+var concatFile = require( 'promise-concat-file' );
+var cPathDestination = './temp/webwelcomer-modules.js';
+var aSourcePaths = [ 
+    "./modules/jquery-widgetfactory/build/jquery-widgetfactory.js",
+    "./modules/util/build/util.js",
+    "./modules/events/build/events.js",
+    "./modules/storage/build/storage.js",
+    "./modules/http/build/http.js",
+    "./modules/visitor/build/visitor.js",
+    "./modules/start-default/build/start-default.js",
+    "./modules/invite-default/build/invite-default.js",
+    "./modules/template/build/template.js",
+    "./modules/actor-default/build/actor-default.js",
+    "./modules/dom/build/dom.js",
+    "./modules/chatbox-live/build/chatbox-live.js",
+    "./modules/presentation-live/build/presentation-live.js"
+];
+
+var oOptions = {
+    prependSourcePath: true,
+    prependDatetime: true,
+    header: '(function( window, undefined ){\n',
+    footer: '\n}( window ));'
+};
+
+concat( aSourcePaths, cPathDestination,  oOptions )
+.then(function( oResult ){
+
+   console.log( 'Success!' );
+
+})
+.fail(function( err ){
+
+    console.log( 'Oops!' );
+    
+});
+```
---
+++ promise-concat-file.js
@@ -0,0 +1,144 @@
+/**
+ * 01-04-2017
+ * NodeJs module that concatenates a list of files.
+ * ~~ Scott Johnson
+ */
+
+
+/** List jshint ignore directives here. **/
+/* jslint node: true */
+/* global JSON:false */
+
+var fs = require( 'fs' );
+var Q = require( 'q' );
+var resolvePath = require( 'promise-resolve-path' );
+
+var concat = module.exports = function( aFiles, cDest, oOptions, undefined ){
+    var deferred = Q.defer();
+    var cPathDest;
+    var cError;
+    var aPathSources;
+    var throwError = function( cError ){
+        deferred.reject( cError );
+    };// /throwError()
+
+    var oSettings = Object.assign({
+        addSourcePath: false,
+        commentBlock: [ '/**', '**/'],
+        header: '',
+        footer: ''
+    }, oOptions );
+
+
+    resolvePath( cDest )
+    .then(function( cResolved ){;
+        cPathDest = cResolved;
+
+        try{
+            fs.writeFileSync( cPathDest, oSettings.header );
+        }
+        catch( e ) {
+            cons
+            throwError( e );
+            return;
+        }
+
+        return resolvePath( aFiles, true );
+        
+    })
+    .then(function( aResolved ){
+        var i, l, aPromises = [];
+        aPathSources = aResolved;
+
+        for( i = 0, l = aPathSources.length; i < l; i++ ) {
+            aPromises.push( readFile( aPathSources[ i ] ) );
+        }// /for()
+        
+        return Q.all( aPromises );
+    })
+    .then( function( aResults ){
+        // All read.
+        var i, l, cContent;
+        var dt = new Date();
+        var err;        
+
+        // Loop over each result and append it's buffer string.
+        for( i = 0, l = aResults.length; i < l; i++ ) {
+            cContent = '';
+
+            if( oSettings.prependSourcePath || oSettings.prependDatetime ){
+                cContent = cContent.concat( '\n', oSettings.commentBlock[ 0 ], '\n' );
+
+                if( oSettings.prependDatetime ){
+                    cContent = cContent.concat( dt, '\n' );
+                }
+
+                if( oSettings.prependSourcePath ){
+                    cContent = cContent.concat( aResults[ i ].path, '\n' );
+                }
+                cContent = cContent.concat( oSettings.commentBlock[ 1 ], '\n' );
+            }
+
+            cContent = cContent.concat( aResults[ i ].buffer.toString( 'utf8' ) );
+            err = appendFileSync( cPathDest, cContent );
+
+            if( err ) {
+                return throwError( err );
+            }
+        }// /for()
+
+        if( oSettings.footer ){
+            err = appendFileSync( cPathDest, oSettings.footer );
+
+            if( err ) {
+                return throwError( err );
+            }
+        }
+
+       deferred.resolve({
+           src: aPathSources,
+           dest: cPathDest
+       });
+    })
+    .fail( function( err ){
+        // One rejected.
+        console.log( 'err' );
+        return throwError( err );
+    });
+
+    return deferred.promise;
+
+};// /concat()
+
+
+
+
+var readFile = function( cFilePath ){
+    var deferred = Q.defer();
+
+    fs.readFile( cFilePath, function ( err, buffer ) {
+        var cContent = buffer.toString( 'utf8' );
+
+        if ( err ){
+            return deferred.reject( err );
+        }
+
+        deferred.resolve({
+            path: cFilePath,
+            buffer: buffer
+        });
+    });
+
+    return deferred.promise;
+};// /readFile()
+
+var appendFileSync = function( cPathDest, cContent ){
+    try{
+        fs.appendFileSync( cPathDest, cContent );
+    }
+    catch( e ) {
+        return e;
+    }
+};// /appendFileSync()
+
+
